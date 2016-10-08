f8d3b1082f5c9a20d7b9ce6c3b6d59494e5d30de
Hiroshi Okagawa <okagawa@gmail.com>
2016-10-08 22:27:05 +0900

    2nd commit

--- FFT_multi1/FFT_multi1/Program.cs
+++ FFT_multi1/FFT_multi1/Program.cs
@@ -11,26 +11,44 @@ namespace FFT_multi1
     {
         private static readonly int np = 3;
         private static readonly int n = 1 << np;
-        private static readonly Complex omega = new Complex(Math.Cos(-2 * Math.PI / n), Math.Sin(-2 * Math.PI / n));
+        private static Complex[] omega;
 
-        Complex[] fft(double[] x)
+        public Program()
         {
-            Complex[] y = new Complex[n];
+            omega = new Complex[n];
 
-            for (int i = 0; i < n; i++)
+            for(int i=0; i < n; i++)
             {
+                omega[i] = new Complex(Math.Cos(-2 * Math.PI / n * i), Math.Sin(-2 * Math.PI / n * i));
             }
 
-            return y;
+        }
+
+
+        static void dft(ref double[] x, ref Complex[] y)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                var t = new Complex(0, 0);
+
+                for (int j = 0; j < n; j++)
+                {
+                    t += new Complex(x[j], 0) * omega[i * j % n];
+                }
+
+                y[i] = t;
+            }
         }
 
         static void Main(string[] args)
         {
             double[] a = { 93.0, 97.0, 58.0, 53.0, 26.0, 59.0, 41.0, 31.0 },
                      b = { 95.0, 27.0, 83.0, 33.0, 64.0, 62.0, 84.0, 23.0 };
-            Complex[] fft_a, fft_b;
+            var dft_a = new Complex[n];
+            var dft_b = new Complex[n];
 
-
+            dft(ref a, ref dft_a);
+            dft(ref b, ref dft_b);
 
         }
     }
